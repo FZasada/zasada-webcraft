a03bb267efb8eba1ece1fa0db8f7c1f9a1ba144f
FlorianZasada <florian.zasada@gmail.com>
2025-09-18 22:33:24 +0200

    Added deploy action

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export', // Required for npm run export
+    images: {
+        unoptimized: true, // GitHub Pages doesn’t support dynamic image optimization
+    },
 };
 
 export default nextConfig;
